86f1b507d7c60c39d97266282435a01463e84cde
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-10 12:11:27 +0530

    auowired & primary &qualifer

--- src/main/java/com/example/datthu/demo/Desktop.java
+++ src/main/java/com/example/datthu/demo/Desktop.java
@@ -1,12 +1,11 @@
 package com.example.datthu.demo;
-
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
-public class Laptop implements Computer {
+public class Desktop implements Computer{
     public void compile(){
-        System.out.println("Laptop is compiling");
+        System.out.println("Desktop is compiling");
     }
 }
--- src/main/java/com/example/datthu/demo/Dev.java
+++ src/main/java/com/example/datthu/demo/Dev.java
@@ -1,27 +1,23 @@
 package com.example.datthu.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
-
-    // Laptop laptop=new Laptop(); manual way of cration of object
-
-    // private Laptop laptop; //by keeping the Laptop component class also Component it gives us null pointer exception
-    // @Autowired  //FIELD INJECTION
-    // private Laptop laptop;
-
-    // public Dev(Laptop laptop){ CONSTRUCTOR INJECTION
-    //     this.laptop = laptop;
-    // }
+//     //here autowired is cheked byType of class
 // @Autowired
-    // private  Laptop laptop;
-    // public  void setLaptop(Laptop laptop){ SETTER INJECTION BUT WE HAVE TO KEPT THE @AUTOWIRED ON IT 
-    //     this.laptop=laptop;
-    // }
+//     private Laptop laptop;
+@Autowired
+@Qualifier("desktop")
+private Computer comp;//here Computer is interface has two class implements it so they are samet TYPE we get this error Field comp in com.example.datthu.demo.Dev required a single bean, but 2 were found: ,because we have kept the component on both class ,so it  has two objects in IOC so it gets confuse to over come 1.delete one class of it or 2.remove @COmponent on one of class or 3.@primary on of the class so it can have the priorty in both of class or 4.use Qualifer("laptop") it checks ByName ,so keep at @Autowired 
+
+// so if you keep both primary it gives error has No qualifying bean of type 'com.example.datthu.demo.Computer' available: more than one 'primary' bean found among candidates: [desktop, laptop]
+// Qualifer has more prefernce then Primary
+// @Autowired should be kept on setter function
     public void build(){
-        // laptop.compile();
+         comp.compile();
         System.out.println("building the application");
     }
 }
