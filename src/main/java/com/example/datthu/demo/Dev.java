e296b4f640593a52968bbb0b4b3bcc048bac5011
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-10 11:49:30 +0530

    types of DI

--- src/main/java/com/example/datthu/demo/Dev.java
+++ src/main/java/com/example/datthu/demo/Dev.java
@@ -1,10 +1,27 @@
 package com.example.datthu.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+
+    // Laptop laptop=new Laptop(); manual way of cration of object
+
+    // private Laptop laptop; //by keeping the Laptop component class also Component it gives us null pointer exception
+    // @Autowired  //FIELD INJECTION
+    // private Laptop laptop;
+
+    // public Dev(Laptop laptop){ CONSTRUCTOR INJECTION
+    //     this.laptop = laptop;
+    // }
+// @Autowired
+    // private  Laptop laptop;
+    // public  void setLaptop(Laptop laptop){ SETTER INJECTION BUT WE HAVE TO KEPT THE @AUTOWIRED ON IT 
+    //     this.laptop=laptop;
+    // }
     public void build(){
+        // laptop.compile();
         System.out.println("building the application");
     }
 }
---
+++ src/main/java/com/example/datthu/demo/Laptop.java
@@ -0,0 +1,10 @@
+package com.example.datthu.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile(){
+        System.out.println("Laptop is compiling");
+    }
+}
